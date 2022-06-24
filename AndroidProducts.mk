a90dda26e839cef8c787999c6d978127dd320c2f
rk134 <rahul-k@bigdi.cc>
2022-06-24 18:53:50 +0100

    vince: AICP-ify
    Signed-off-by: rk134 <rahul-k@bigdi.cc>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_vince.mk
+    $(LOCAL_DIR)/aicp_vince.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_vince-user \
-    aosp_vince-userdebug \
-    aosp_vince-eng
+    aicp_vince-user \
+    aicp_vince-userdebug \
+    aicp_vince-eng
--- aicp_vince.mk
+++ aicp_vince.mk
@@ -27,20 +27,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common AICP stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 # Build Flags
 TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GMS := true
 TARGET_FACE_UNLOCK := true
 TARGET_USES_BLUR := false
 TARGET_USES_AOSP_CHARGER := true
 TARGET_SUPPORTS_QUICK_TAP := false
 TARGET_DISABLE_POSTRENDER_CLEANUP := true
+BUILD_TYPE := unofficial
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := aosp_vince
+PRODUCT_NAME := aicp_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
